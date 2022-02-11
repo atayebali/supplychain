c29202c2791bae63673938dd94f5eb8924265052
Anis Tayebali <atayebalI@gmail.com>
2022-02-11 02:44:51 +0000

    Deployed Code to Rinkeby

--- truffle.js
+++ truffle.js
@@ -1,9 +1,18 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+var words = "words"
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      // rinkeby link taken from metamask
+      provider: () => new HDWalletProvider(words, "https://rinkeby.infura.io/v3/KEY"),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,
+      gasPrice: 10000000000,     // Ropsten has a lower block limit than mainnet      
     }
   }
 };
